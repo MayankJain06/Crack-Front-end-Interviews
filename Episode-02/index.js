90d534a7f3ce654afebce26b774eccd11977e1b7
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-01-13 23:58:57 +0530

    Update index.js

--- Episode-02/index.js
+++ Episode-02/index.js
@@ -9,16 +9,24 @@ let printFullName= function (hometown, state){
 
 // bind method : The bind method will return copy of the method 
 
-let myName=printFullName.bind(name1, "Hyderabad", "Telangana");
+let myName=printFullName.bind(name1, "Hyderabad");
 console.log(myName); // this will return copy of the method 
-myName();
+myName( "Telangana");
 
 // Creating our own bind method
 
-Function.prototype.mybind= function(...arguments){
+Function.prototype.mybind= function(...arguments) {
+    // this will point to printFullName
     let obj= this;
-    return function(){
-
-
+    console.log(arguments);
+    params=arguments.slice(1);
+    // the params which we receive are in the form of arrays hence we are using apply method below since it takes array of arguments as params
+    return function(...args2) {
+    obj.apply(arguments[0], [...params, ...args2]);
+ 
     }
 }
+
+let myName2=printFullName.mybind(name1, "Hyderabad");
+console.log(myName); // this will return copy of the method 
+myName2("Telangana");
