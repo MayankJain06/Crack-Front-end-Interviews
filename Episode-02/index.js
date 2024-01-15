adfea82aa9d6f6c5c63c8c330035e4aa10067ca7
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-01-15 20:00:35 +0530

    Update index.js

--- Episode-02/index.js
+++ Episode-02/index.js
@@ -21,11 +21,11 @@ Function.prototype.mybind= function(...args) {
     params=args.slice(1);
     // the params which we receive are in the form of arrays hence we are using apply method below since it takes array of arguments as params
     return function(...args2) {
-    obj.apply(args[0], [...params, ...args2]);
+    obj.apply(args[0], [...params, ...args2]); //aaply method will take array of objects as parameter
  
     }
 }
 
 let myName2=printFullName.mybind(name1, "Hyderabad");
 console.log(myName); // this will return copy of the method 
-myName2("Telangana");
+myName2("Telangana"); 
